9f51d21ec5696f6bc0bf939a09feeee6a0af6bf8
Isuzuri <ballage94@gmail.com>
2024-06-18 23:03:21 +0300

    add modal window (without functions)

--- src/components/main/blog.jsx
+++ src/components/main/blog.jsx
@@ -1,7 +1,21 @@
-import { Col, Row } from 'antd';
-import { Button, Flex, Card } from "antd";
+import React, {useState} from "react";
+import { Button, Flex, Card, Modal, Col, Row, Input, TextArea } from "antd";
 
 export default function Blog() {
+    const [open, setOpen] = useState(false)
+
+    const showModal = () => {
+        setOpen(true)
+    }
+
+    const handleOk = () => {
+        setOpen(false)
+    }
+
+    const handleCancel = () => {
+        setOpen(false)
+    }
+
     const posts = [
         {
             title: 'First Test',
@@ -9,15 +23,31 @@ export default function Blog() {
         }
     ]
 
+    const { TextArea } = Input;
+
     return <div>
         <Row>
             <Col span={20}>
-                {posts.map((post) => {
-                    return <Card title={post.title}>{post.description}</Card>
+                {posts.map((post, index) => {
+                    return <Card title={post.title} key={index}>{post.description}</Card>
                 })}
             </Col>
             <Col span={4}>
-                <Flex justify='center'><Button type='primary' style={{width:'90%'}}>Create Post</Button></Flex>
+                <Flex justify='center'>
+                    <Button type='primary' style={{width:'90%'}} onClick={showModal}>Create Post</Button>
+                    <Modal 
+                        title='New Post' 
+                        open={open}
+                        onOk={handleOk}
+                        onCancel={handleCancel}
+                        okText={'Send'}
+                    >
+                        <Flex vertical gap='middle'>
+                            <Input placeholder="Title"></Input>
+                            <TextArea placeholder="Description" allowClear autoSize={{minRows: 5}}></TextArea>
+                        </Flex>
+                    </Modal>
+                </Flex>
             </Col>
         </Row>
     </div>
